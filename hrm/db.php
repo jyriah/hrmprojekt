12e92f69eacdf04b8c7afbb26f7bd7229c1d1699
Jüri Ahhundov <jyriah@gmail.com>
2017-05-27 11:34:39 +0300

    testin andmebaasi ühendust

---
+++ hrm/db.php
@@ -0,0 +1,23 @@
+<?php
+require_once("dbconf.php");
+$conn=mysqli_connect($host, $user, $password, $db);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  $sql = "SELECT id, firstname, lastname FROM person";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo "Isiku ID: " . $row["id"]. " - Eesnimi: " . $row["firstname"]. ", Perenimi: " . $row["lastname"]. "<br>";
+      }
+  } else {
+      echo "0 results";
+  }
+
+mysqli_close($conn);
+?>
