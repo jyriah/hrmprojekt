ce07cc8be4e5b27caf02d2fb660eabad8b213ac1
Jüri Ahhundov <jyri@metaprint.com>
2017-05-29 16:39:51 +0300

    töötajate kuvamise tabel silutud

--- hrm/views/employees.php
+++ hrm/views/employees.php
@@ -16,36 +16,33 @@ if (mysqli_connect_errno())
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
-      echo "<table>
-        <tr>
-          <th>Jrk.</th>
-          <th>Eesnimi</th>
-          <th>Perenimi</th>
-          <th>E-mail</th>
-          <th>Osakond</th>
-          <th>Amet</th>
-        </tr>";
+	  echo "<table>";
+	  echo "<tr>";
+	  echo "<th>Jrk.</th>";
+	  echo "<th>Eesnimi</th>";
+	  echo "<th>Perenimi</th>";
+	  echo "<th>Email</th>";
+	  echo "<th>Osakond</th>";
+	  echo "<th>Amet</th>";
+	  echo "</tr>";
 
       // output data of each row
       while($row = $result->fetch_assoc()) {
-          echo "<tr><td>"
-          . $order_number. "</td><td>"
-          . $row["firstname"]. "</td><td>"
-          . $row["lastname"]. "</td><td>"
-          . $row["email"] . "</td><td>"
-          . $row["department"] . "</td><td>"
-          . $row["position"] . "</td>"
-          . "<td class='button-cell'>"
-          . "<button class='edit-button' onclick='editEmployee("
-          . $row["id"]
-          . ")'>Muuda</button></td><td class='button-cell'>"
-          . "<button onclick='deleteEmployee("
-          . $row["id"]. ")'>Arhiivi</button>"
-          . "</td></tr>";
-          $order_number++;
+		  echo "<tr>";
+		  echo "<td>" . $order_number . "</td>";
+		  echo "<td>" . $row["firstname"] . "</td>";
+		  echo "<td>" . $row["lastname"] . "</td>";
+		  echo "<td>" . $row["email"] . "</td>";
+		  echo "<td>" . $row["department"] . "</td>";
+		  echo "<td>" . $row["position"] . "</td>";
+		  echo "<td class='button-cell'><button class='edit-button' onclick='editEmployee(". $row["id"] . ")'>Muuda</button></td>";
+		  echo "<td class='button-cell'><button onclick='deleteEmployee(" . $row["id"] . ")'>Arhiivi</button></td>";
+		  echo "</tr>";
+		  $order_number++;
       }
-      echo "</table>";
+	  echo "</table>";
 	  echo "<div><button class='add-button' onclick='addEmployee()'>Lisa töötaja</button></div>";
+	  
   } else {
       echo "Päringule ei leitud ühtegi vastust.";
   }
